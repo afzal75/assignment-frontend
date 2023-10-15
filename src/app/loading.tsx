27c868950be80ec82c8e7b8b3fcfd182be83d772
afzal75 <afzalbhuiyan16@gmail.com>
2023-10-15 11:35:01 +0600

    reuseable form component setup successfully

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,9 +2,9 @@
 const Loading = () => {
     return (
         <div className='flex justify-center items-center h-screen'>
-          <p className='text-7xl font-thin'>L</p>
+          <p className='text-7xl font-bold'>L</p>
           <div className='w-10 h-10 border-8 border-dashed rounded-full animate-spin mt-5 border-green-400'></div>
-          <p className='text-7xl font-thin'>ading....</p>
+          <p className='text-7xl font-bold'>ading....</p>
         </div>
       )
 };
---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+const LoginPage = () => {
+    return (
+        <div>
+            <h1>YYYY</h1>
+        </div>
+    );
+};
+
+export default LoginPage;
