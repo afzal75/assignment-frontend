dd67a0b8c639cc0501ec829b10b8eeeb055be3a6
afzal75 <afzalbhuiyan16@gmail.com>
2023-10-17 22:48:42 +0600

    latest news section completed successfully

--- src/app/(withlayout)/home/page.tsx
+++ src/app/(withlayout)/home/page.tsx
@@ -1,12 +1,16 @@
 
 import Cart from '@/components/ui/Cart';
 import Header from '@/components/ui/Header';
+import LatestNews from '@/components/ui/LatestNews';
+import Services from '@/components/ui/Services';
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div>
       <Header></Header>
+      <Services></Services>
+      <LatestNews></LatestNews>
       <Cart></Cart>
     </div>
   );
