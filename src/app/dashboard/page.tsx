a7f12df32b0031096de8e891ae679bcb9af65ec4
afzal75 <afzalbhuiyan16@gmail.com>
2023-10-18 23:19:47 +0600

    feedback created successfully

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import DashboardServicePage from './(dashBoardLayout)/blog/page';
 import ServicePage from './(dashBoardLayout)/service/page';
+// import FeedbackPage from './(dashBoardLayout)/feedback/[id]/page';
 
 const DashboardPage = () => {
     return (
@@ -8,6 +9,7 @@ const DashboardPage = () => {
             <h1>Dashboard Page</h1>
             <DashboardServicePage></DashboardServicePage>
             <ServicePage></ServicePage>
+            {/* <FeedbackPage params={id}></FeedbackPage> */}
         </div>
     );
 };
