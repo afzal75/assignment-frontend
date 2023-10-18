5b06dddaf6f74c26381177a7d122a2523c643d2f
afzal75 <afzalbhuiyan16@gmail.com>
2023-10-18 21:32:18 +0600

    service create and edit successfully done

---
+++ src/app/dashboard/(dashBoardLayout)/service/create/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import CreateServices from "@/components/dashBoard/ui/createService";
+import React from "react";
+
+const CreateService = () => {
+  return (
+    <div>
+      <CreateServices />
+    </div>
+  );
+};
+
+export default CreateService;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import DashboardServicePage from './(dashBoardLayout)/blog/page';
+import ServicePage from './(dashBoardLayout)/service/page';
 
 const DashboardPage = () => {
     return (
         <div>
             <h1>Dashboard Page</h1>
             <DashboardServicePage></DashboardServicePage>
-
+            <ServicePage></ServicePage>
         </div>
     );
 };
